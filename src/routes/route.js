5c92762a3cfe1e6d62cf724b32b0c67b10986626
RinkiPradhan6210 <rinkipradhan6210@gmail.com>
2022-06-02 03:21:42 +0530

    not to add

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const externalModule = require('./logger')
+//const externalModule = require('../logger/logger')
 
 const router = express.Router();
+const rinki =require('../logger/logger')
+const date = require('../util/helper')
+const trim = require('../validator/formatter')
+
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
-});
-
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
+    //res.send('My latest and this first api!')
+    rinki.welcome()
+    date.mydata()
+    date.mymonth()
+    date.getbatchinfo()
+    trim.trim()
+    trim.changetoLowerCase()
+    trim.changetoUpperCase()
 });
 
 module.exports = router;
