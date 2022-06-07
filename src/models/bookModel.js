5b8f6be5d70590895cc2943904209af2ee487800
RinkiPradhan6210 <rinkipradhan6210@gmail.com>
2022-06-08 04:08:48 +0530

    mongo-s3

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema = new mongoose.Schema({
+    name:{
+         type: String 
     },
-    sales: {type: Number, default: 10},
+    author_id:{
+        type: String, 
+        required:true
+    },
+    prices: Number,
+    ratings:Number
+},{ timestamps: true });
+
+module.exports = mongoose.model('Book', bookSchema)//books
+
+    // tags: [String],
+    
+    // isPublished: Boolean,
+    // prices: {
+    //     indianPrice: String,
+    //     europePrice: String,
+    // },
+    // sales: {type: Number, default: 10},
     
     // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
     // {
@@ -18,10 +29,10 @@ const bookSchema = new mongoose.Schema( {
         // "ch2" : "intro to nodejs",
         // "ch3" : "intro to db"
     //  }
-    summary :  mongoose.Schema.Types.Mixed,
-    isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
-
-}, { timestamps: true });
+    // summary :  mongoose.Schema.Types.Mixed,
+    // isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+
+
+ //users
